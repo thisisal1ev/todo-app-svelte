5690fd0ed1fbce4f519cd4e5f835546bd3452876
thisisal1ev <aaalievvv1@gmail.com>
2025-01-09 22:41:52 +0500

    Add Checkbox component and refactor Todos; integrate search and filter functionality in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,45 @@
 <script lang="ts">
-	import { SearchIcon, SunIcon } from './components'
+	import { derived, writable, type Writable } from 'svelte/store'
+	import { PlusIcon, SearchIcon, SunIcon } from './components'
 	import Todos from './components/Todos.svelte'
 
-	const storedTheme = localStorage.getItem('theme')
-	let isDarkMode: string = storedTheme === 'dark' ? 'dark' : 'light'
-	const todos: {
+	interface Todo {
 		id: number
 		title: string
 		completed: boolean
-	}[] = $state([
+	}
+
+	type Filter = 'all' | 'completed' | 'uncompleted'
+
+	let searchValue = writable('')
+	let selectedFilter: Writable<Filter> = writable('all')
+	const storedTheme: string | null = localStorage.getItem('theme')
+	let isDarkMode: string = $state(storedTheme === 'dark' ? 'dark' : 'light')
+	const todos = writable<Todo[]>([
 		{ id: 1, title: 'Note #1', completed: false },
 		{ id: 2, title: 'Note #2', completed: true },
 		{ id: 3, title: 'Note #3', completed: false },
 	])
 
-	const toggleDarkMode = () => {
+	const displayedTodos = derived(
+		[todos, searchValue, selectedFilter],
+		([$todos, $searchValue, $selectedFilter]) => {
+			return $todos.filter((todo: Todo) => {
+				const matchesSearch = todo.title
+					.toLowerCase()
+					.trim()
+					.includes($searchValue.toLowerCase().trim())
+
+				const matchesFilter =
+					$selectedFilter === 'all' ||
+					($selectedFilter === 'completed' && todo.completed) ||
+					($selectedFilter === 'uncompleted' && !todo.completed)
+				return matchesSearch && matchesFilter
+			})
+		}
+	)
+
+	const toggleDarkMode = (): void => {
 		if (isDarkMode === 'light') {
 			document.documentElement.classList.add('dark')
 			localStorage.setItem('theme', 'dark')
@@ -44,6 +69,11 @@
 						class="relative w-full lg:w-full py-2 h-10 pl-4 pr-10 rounded-md outline-none bg-white border-2 border-violet dark:border-grey dark:bg-mainBlack placeholder:font-medium placeholder:text-base placeholder:leading-4 placeholder:text-[#C3C1E5] placeholder:dark:text-[#666] placeholder:select-none focus:dark:border-violet transition-colors duration-300"
 						type="search"
 						placeholder="Search note..."
+						oninput={(event: Event) => {
+							searchValue.update(
+								() => (event.target as HTMLInputElement)?.value
+							)
+						}}
 					/>
 					<SearchIcon
 						class="absolute right-4 top-2 transition-colors duration-300 fill-violet"
@@ -53,6 +83,10 @@
 				<div class="flex justify-between items-center space-x-5 lg:space-x-4">
 					<select
 						class="rounded-md h-10 bg-violet text-grey px-2 py-1 lg:w-28 outline-none uppercase font-medium text-xs"
+						onchange={(event: Event) =>
+							selectedFilter.update(
+								() => (event.target as HTMLSelectElement)?.value as Filter
+							)}
 					>
 						<option value="all">All</option>
 						<option value="completed">Completed</option>
@@ -73,6 +107,41 @@
 			</div>
 		</div>
 
-		<Todos {todos} />
+		{#if $displayedTodos.length > 0}
+			<Todos todos={$displayedTodos} />
+		{:else}
+			<div class="flex items-center pt-10 flex-col select-none space-y-5">
+				{#if isDarkMode === 'dark'}
+					<img
+						src="./img/detective-dark.png"
+						alt="detective dark"
+						width="200"
+						height="170"
+					/>
+				{:else}
+					<img
+						src="./img/detective.png"
+						alt="detective"
+						width="200"
+						height="170"
+					/>
+				{/if}
+				<p
+					class="text-xl leading-5 font-normal text-center text-black dark:text-white transition-colors duration-300"
+				>
+					Empty...
+				</p>
+			</div>
+		{/if}
 	</section>
 </main>
+
+<button
+	title="modal btn"
+	aria-label="Open modal"
+	class="bg-violet border-2 border-violet fixed bottom-8 right-8 md:right-20 lg:right-[12%] xl:right-[25%] p-3 rounded-full active:bg-grey transition-colors duration-300 outline-none group"
+>
+	<PlusIcon
+		class="fill-[#f7f7f7] group-active:fill-violet transition-colors duration-300"
+	/>
+</button>
